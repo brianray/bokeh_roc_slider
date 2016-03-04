5460decc6932572fca0e467b07f61ba1ecd84266
Bryan Van de Ven <bryanv@continuum.io>
2016-03-04 14:07:23 -0600

    updates for Bokeh >= 0.11, Python 3

--- roc/pyroc.py
+++ roc/pyroc.py
@@ -15,19 +15,20 @@ Python Module for calculating the area under the receive operating characteristi
 0.2 - Updated the code by adding new metrics for analysis with the confusion matrix.
 
 """
+from __future__ import print_function
 
 import random
 import math
 try:
 	import pylab
 except:
-	print "error:\tcan't import pylab module, you must install the module:\n"
-	print "\tmatplotlib to plot charts!'\n"
+	print("error:\tcan't import pylab module, you must install the module:\n")
+	print("\tmatplotlib to plot charts!'\n")
 
 
 def random_mixture_model(pos_mu=.6,pos_sigma=.1,neg_mu=.4,neg_sigma=.1,size=200):
-	pos = [(1,random.gauss(pos_mu,pos_sigma),) for x in xrange(size/2)]
-	neg = [(0,random.gauss(neg_mu,neg_sigma),) for x in xrange(size/2)]
+	pos = [(1,random.gauss(pos_mu,pos_sigma),) for x in list(range(int(size/2)))]
+	neg = [(0,random.gauss(neg_mu,neg_sigma),) for x in list(range(int(size/2)))]
 	return pos+neg
 
 
@@ -44,26 +45,26 @@ def plot_multiple_rocs_separate(rocList,title='', labels = None, equal_aspect = 
 		if equal_aspect:
 			cax = pylab.gca()
 			cax.set_aspect('equal')
-		
+
 		if not labels:
 			labels = ['' for x in rocList]
-		
+
 		pylab.text(0.2,0.1,labels[ix],fontsize=8)
 		pylab.plot([x[0] for x in r.derived_points],[y[1] for y in r.derived_points], 'r-',linewidth=2)
-	
+
 	pylab.show()
-	
+
 
 
 def _remove_duplicate_styles(rocList):
- 	""" Checks for duplicate linestyles and replaces duplicates with a random one."""
+	""" Checks for duplicate linestyles and replaces duplicates with a random one."""
 	pref_styles = ['cx-','mx-','yx-','gx-','bx-','rx-']
 	points = 'ov^>+xd'
 	colors = 'bgrcmy'
 	lines = ['-','-.',':']
-	
+
 	rand_ls = []
-	
+
 	for r in rocList:
 		if r.linestyle not in rand_ls:
 			rand_ls.append(r.linestyle)
@@ -81,11 +82,11 @@ def _remove_duplicate_styles(rocList):
 						r.linestyle = ls
 						rand_ls.append(ls)
 						break
-						
+
 
 
 def plot_multiple_roc(rocList,title='',labels=None, include_baseline=False, equal_aspect=True):
-	""" Plots multiple ROC curves on the same chart. 
+	""" Plots multiple ROC curves on the same chart.
 		Parameters:
 			rocList: the list of ROCData objects
 			title: The tile of the chart
@@ -114,12 +115,12 @@ def plot_multiple_roc(rocList,title='',labels=None, include_baseline=False, equa
 		pylab.plot([0.0,1.0], [0.0, 1.0], 'k-', label= 'random')
 	if labels:
 		pylab.legend(loc='lower right')
-		
+
 	pylab.show()
 
 
 def load_decision_function(path):
-	""" Function to load the decision function (DataSet) 
+	""" Function to load the decision function (DataSet)
 		Parameters:
 			path: The dataset file path
 		Return:
@@ -136,7 +137,7 @@ def load_decision_function(path):
 	fileHandler.close()
 
 	return model_data
-	
+
 
 class ROCData(object):
 	""" Class that generates an ROC Curve for the data.
@@ -154,23 +155,23 @@ class ROCData(object):
 					  t[1] = a score
 			 		  t[2] = any label (optional)
 				lineStyle: THe matplotlib style string for plots.
-				
-			Note: The ROCData is still usable w/o matplotlib. The AUC is still available, 
+
+			Note: The ROCData is still usable w/o matplotlib. The AUC is still available,
 			      but plots cannot be generated.
 		"""
-		self.data = sorted(data,lambda x,y: cmp(y[1],x[1]))
+		self.data = sorted(data, key=lambda x: x[1])
 		self.linestyle = linestyle
 		self.auc() #Seed initial points with default full ROC
-	
+
 	def auc(self,fpnum=0):
-		""" Uses the trapezoidal ruel to calculate the area under the curve. If fpnum is supplied, it will 
+		""" Uses the trapezoidal ruel to calculate the area under the curve. If fpnum is supplied, it will
 			calculate a partial AUC, up to the number of false positives in fpnum (the partial AUC is scaled
 			to between 0 and 1).
 			It assumes that the positive class is expected to have the higher of the scores (s(+) < s(-))
 			Parameters:
 				fpnum: The cumulativr FP count (fps)
 			Return:
-			
+
 		"""
 		fps_count = 0
 		relevant_pauc = []
@@ -189,7 +190,7 @@ class ROCData(object):
 				current_index +=1
 		total_n = len([x for x in relevant_pauc if x[0] == 0])
 		total_p = len(relevant_pauc) - total_n
-		
+
 		#Convert to points in a ROC
 		previous_df = -1000000.0
 		current_index = 0
@@ -211,7 +212,7 @@ class ROCData(object):
 		points.append((fpr,tpr,fp_count)) #Add last point
 		points.sort(key=lambda i: (i[0],i[1]))
 		self.derived_points = points
-		
+
 		return self._trapezoidal_rule(points)
 
 
@@ -223,7 +224,7 @@ class ROCData(object):
 			next_pt = curve_pts[ix+1]
 			cum_area += ((cur_pt[1]+next_pt[1])/2.0) * (next_pt[0]-cur_pt[0])
 		return cum_area
-		
+
 	def calculateStandardError(self,fpnum=0):
 		""" Returns the standard error associated with the curve.
 			Parameters:
@@ -232,29 +233,29 @@ class ROCData(object):
 				the standard error.
 		"""
 		area = self.auc(fpnum)
-		
+
 		#real positive cases
 		Na =  len([ x for x in self.data if x[0] == 1])
-		
+
 		#real negative cases
 		Nn =  len([ x for x in self.data if x[0] == 0])
-		
-		
+
+
 		Q1 = area / (2.0 - area)
 		Q2 = 2 * area * area / (1.0 + area)
-		
+
 		return math.sqrt( ( area * (1.0 - area)  +   (Na - 1.0) * (Q1 - area*area) +
 						(Nn - 1.0) * (Q2 - area * area)) / (Na * Nn))
-							
-	
+
+
 	def plot(self,title='',include_baseline=False,equal_aspect=True):
-		""" Method that generates a plot of the ROC curve 
+		""" Method that generates a plot of the ROC curve
 			Parameters:
 				title: Title of the chart
 				include_baseline: Add the baseline plot line if it's True
 				equal_aspect: Aspects to be equal for all plot
 		"""
-		
+
 		pylab.clf()
 		pylab.plot([x[0] for x in self.derived_points], [y[1] for y in self.derived_points], self.linestyle)
 		if include_baseline:
@@ -270,10 +271,10 @@ class ROCData(object):
 		pylab.xlabel('1 - Specificity')
 		pylab.ylabel('Sensitivity')
 		pylab.title(title)
-		
+
 		pylab.show()
-		
-	
+
+
 	def confusion_matrix(self,threshold,do_print=False):
 		""" Returns the confusion matrix (in dictionary form) for a fiven threshold
 			where all elements > threshold are considered 1 , all else 0.
@@ -287,14 +288,14 @@ class ROCData(object):
 		neg_points = [x for x in self.data if x[1] < threshold]
 		tp,fp,fn,tn = self._calculate_counts(pos_points,neg_points)
 		if do_print:
-			print "\t Actual class"
-			print "\t+(1)\t-(0)"
-			print "+(1)\t%i\t%i\tPredicted" % (tp,fp)
-			print "-(0)\t%i\t%i\tclass" % (fn,tn)
+			print("\t Actual class")
+			print("\t+(1)\t-(0)")
+			print("+(1)\t%i\t%i\tPredicted" % (tp,fp))
+			print("-(0)\t%i\t%i\tclass" % (fn,tn))
 		return {'TP': tp, 'FP': fp, 'FN': fn, 'TN': tn}
-		
 
-	
+
+
 	def evaluateMetrics(self,matrix,metric=None,do_print=False):
 		""" Returns the metrics evaluated from the confusion matrix.
 			Parameters:
@@ -305,35 +306,35 @@ class ROCData(object):
 				the dictionary with the Accuracy, Sensitivity, Specificity,Efficiency,
 				                        PositivePredictiveValue, NegativePredictiveValue, PhiCoefficient
 		"""
-		
+
 		accuracy = (matrix['TP'] + matrix['TN'])/ float(sum(matrix.values()))
-		
+
 		sensitivity = (matrix['TP'])/ float(matrix['TP'] + matrix['FN'])
-		
+
 		specificity = (matrix['TN'])/float(matrix['TN'] + matrix['FP'])
-		
+
 		efficiency = (sensitivity + specificity) / 2.0
-		
+
 		positivePredictiveValue =  matrix['TP'] / float(matrix['TP'] + matrix['FP'])
 
 		NegativePredictiveValue = matrix['TN'] / float(matrix['TN'] + matrix['FN'])
-		
+
 		PhiCoefficient = (matrix['TP'] * matrix['TN'] - matrix['FP'] * matrix['FN'])/(
 							math.sqrt( (matrix['TP'] + matrix['FP']) *
 							           (matrix['TP'] + matrix['FN']) *
 									   (matrix['TN'] + matrix['FP']) *
 									   (matrix['TN'] + matrix['FN']))) or 1.0
-									
+
 		if do_print:
-			print 'Sensitivity: ' , sensitivity
-			print 'Specificity: ' , specificity
-			print 'Efficiency: ' , efficiency
-			print 'Accuracy: ' , accuracy
-			print 'PositivePredictiveValue: ' , positivePredictiveValue
-			print 'NegativePredictiveValue' , NegativePredictiveValue
-			print 'PhiCoefficient' , PhiCoefficient
-			
-		
+			print('Sensitivity: ' , sensitivity)
+			print('Specificity: ' , specificity)
+			print('Efficiency: ' , efficiency)
+			print('Accuracy: ' , accuracy)
+			print('PositivePredictiveValue: ' , positivePredictiveValue)
+			print('NegativePredictiveValue' , NegativePredictiveValue)
+			print('PhiCoefficient' , PhiCoefficient)
+
+
 		return {'SENS': sensitivity, 'SPEC': specificity, 'ACC': accuracy, 'EFF': efficiency,
 				'PPV':positivePredictiveValue, 'NPV':NegativePredictiveValue , 'PHI':  PhiCoefficient}
 
@@ -345,21 +346,21 @@ class ROCData(object):
 		fn_count = len([x for x in neg_data if x[0] == 1])
 		tn_count = len([x for x in neg_data if x[0] == 0])
 		return tp_count,fp_count,fn_count, tn_count
-		
 
-		
+
+
 if __name__ == '__main__':
-	print "PyRoC - ROC Curve Generator"
-	print "By Marcel Pinheiro Caraciolo (@marcelcaraciolo)"
-	print "http://aimotion.bogspot.com\n"
+	print("PyRoC - ROC Curve Generator")
+	print("By Marcel Pinheiro Caraciolo (@marcelcaraciolo)")
+	print("http://aimotion.bogspot.com\n")
 	from optparse import OptionParser
-	
+
 	parser = OptionParser()
 	parser.add_option('-f', '--file', dest='origFile', help="Path to a file with the class and decision function. The first column of each row is the class, and the second the decision score.")
 	parser.add_option("-n", "--max fp", dest = "fp_n", default=0, help= "Maximum false positives to calculate up to (for partial AUC).")
 	parser.add_option("-p","--plot", action="store_true",dest='plotFlag', default=False, help="Plot the ROC curve (matplotlib required)")
 	parser.add_option("-t",'--title', dest= 'ptitle' , default='' , help = 'Title of plot.')
-	
+
 	(options,args) = parser.parse_args()
 
 
@@ -370,13 +371,12 @@ if __name__ == '__main__':
 	df_data = load_decision_function(options.origFile)
 	roc = ROCData(df_data)
 	roc_n = int(options.fp_n)
-	print "ROC AUC: %s" % (str(roc.auc(roc_n)),)
-	print 'Standard Error:  %s' % (str(roc.calculateStandardError(roc_n)),) 
-	
-	print ''
+	print("ROC AUC: %s" % (str(roc.auc(roc_n)),))
+	print('Standard Error:  %s' % (str(roc.calculateStandardError(roc_n)),))
+
+	print('')
 	for pt in roc.derived_points:
-		print pt[0],pt[1]
-		
+		print(pt[0],pt[1])
+
 	if options.plotFlag:
 		roc.plot(options.ptitle,True,True)
-		
